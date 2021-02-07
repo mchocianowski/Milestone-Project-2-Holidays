32410347f2562b7a77e2aeff6bd161a3db44a070
mchocianowski <ch.mateusz@hotmail.com>
2021-02-07 09:27:42 +0000

    Add all objects to the array. Format document.

--- assets/js/holiday-finder.js
+++ assets/js/holiday-finder.js
@@ -1,59 +1,201 @@
-let hotDestinations = ['Gran Canaria', 'Ibiza', 'Morroco', 'Tenerife', 'Mallorca'];
-let coldDestinations = ['Zakopane', 'Poland', 'Innsbruch', 'Austria', 'Charmonix, France', 'Beitostolen, Norway'];
-let cityBreaks = ['Paris-France', 'Prague-Czech Republic', 'Berlin-Germany', 'Warsaw-Poland', 'Poznan-Poland'];
-let natureDestinations = ['Chile', 'Madagascar', 'New Zealand', 'Ecuador', 'Costa Rica'];
-let errorTxt = ['Sorry we cannot find any destinations for you at this time, please check back at a later date for more holiday destinations'];
 let destinations = [
     {
-        name: 'Gran Canaria',
-        climate:'hot',
-        season:'summer',
+        name: 'Ibiza',
+        climate: 'hot',
+        season: 'summer',
         type: 'beach',
         location: {
-            lat: 40.785091,
-            lng: -73.968285
+            lat: 39.020012,
+            lng: 1.482148
         }
     },
     {
-        name: 'Ibiza',
-        climate:'hot',
-        season:'summer',
+        name: 'Gran Canaria',
+        climate: 'hot',
+        season: 'summer',
         type: 'beach',
         location: {
-        lat: 41.084045,
-        lng: -73.874256
-    }
+            lat: 28.124830,
+            lng: -15.430020
+        }
+    },
+    {
+        name: 'Morroco',
+        climate: 'hot',
+        season: 'summer',
+        type: 'beach',
+        location: {
+            lat: 31.791702,
+            lng: -7.092620
+        }
+    },
+    {
+        name: 'Tenerife',
+        climate: 'hot',
+        season: 'summer',
+        type: 'beach',
+        location: {
+            lat: 28.291565,
+            lng: -16.629129
+        }
+    },
+    {
+        name: 'Mallorca',
+        climate: 'hot',
+        season: 'summer',
+        type: 'beach',
+        location: {
+            lat: 39.695263,
+            lng: 3.017571
+        }
+    },
+    {
+        name: 'Innsbruck, Austria',
+        climate: 'cold',
+        season: 'winter',
+        type: 'ski',
+        location: {
+            lat: 47.2654296,
+            lng: 11.3927685
+        }
     },
     {
         name: 'Zakopane, Poland',
-        climate:'cold',
-        season:'winter',
+        climate: 'cold',
+        season: 'winter',
         type: 'ski',
         location: {
-        lat: 40.754932,
-        lng: -73.984016
-    }
+            lat: 49.2969446,
+            lng: 19.950659
+        }
+    },
+    {
+        name: 'Charmonix, France',
+        climate: 'cold',
+        season: 'winter',
+        type: 'ski',
+        location: {
+            lat: 45.9237,
+            lng: 6.8694
+        }
+    },
+    {
+        name: 'Beitostolen, Norway',
+        climate: 'cold',
+        season: 'winter',
+        type: 'ski',
+        location: {
+            lat: 61.2498658,
+            lng: 8.9064205
+        }
+    },
+    {
+        name: 'Paris-France',
+        climate: 'hot or cold',
+        season: 'both',
+        type: 'city',
+        location: {
+            lat: 48.8566969,
+            lng: 2.3514616
+        }
+    },
+    {
+        name: 'Prague-Czech Republic',
+        climate: 'hot or cold',
+        season: 'both',
+        type: 'city',
+        location: {
+            lat: 50.0874654,
+            lng: 14.4212535
+        }
+    },
+    {
+        name: 'Berlin-Germany',
+        climate: 'hot or cold',
+        season: 'both',
+        type: 'city',
+        location: {
+            lat: 52.5170365,
+            lng: 13.3888599
+        }
+    },
+    {
+        name: 'Warsaw-Poland',
+        climate: 'hot or cold',
+        season: 'both',
+        type: 'city',
+        location: {
+            lat: 52.2319581,
+            lng: 21.0067249
+        }
+    },
+    {
+        name: 'Poznan-Poland',
+        climate: 'hot or cold',
+        season: 'both',
+        type: 'city',
+        location: {
+            lat: 52.4006632,
+            lng: 16.9197326
+        }
+    },
+    {
+        name: 'Chile',
+        climate: 'hot',
+        season: 'summer',
+        type: 'nature',
+        location: {
+            lat: -31.7613365,
+            lng: -71.3187697
+        }
+    },
+    {
+        name: 'Madagascar',
+        climate: 'hot',
+        season: 'summer',
+        type: 'nature',
+        location: {
+            lat: -18.9249604,
+            lng: 46.4416422
+        }
+    },
+    {
+        name: 'New Zealand',
+        climate: 'hot',
+        season: 'summer',
+        type: 'nature',
+        location: {
+            lat: -41.5000831,
+            lng: 172.8344077
+        }
+    },
+    {
+        name: 'Ecuador',
+        climate: 'hot',
+        season: 'summer',
+        type: 'nature',
+        location: {
+            lat: -1.3397668,
+            lng: -79.3666965
+        }
+    },
+    {
+        name: 'Costa Rica',
+        climate: 'hot',
+        season: 'summer',
+        type: 'nature',
+        location: {
+            lat: 10.2735633,
+            lng: -84.0739102
+        }
     },
 
-]
-var hotDestinationsLocations = [
-    {
-        lat: 40.785091,
-        lng: -73.968285
-    },
-    {
-        lat: 41.084045,
-        lng: -73.874256
-    },
-    {
-        lat: 40.754932,
-        lng: -73.984016
-    }
+
 ];
-
+//Global variable for map.
 var map;
 
-
+//Set up the google maps api, setting the center to the middle of the Atlantic ocean.
 function initMap() {
     const center = { lat: 46.619261, lng: -33.134766 };
     map = new google.maps.Map(document.getElementById("map"), {
@@ -68,14 +210,19 @@ function validateForm() {
     var type = document.getElementById("type").value;
     var season = document.getElementById("season").value;
 
-        const selectedDestinations = destinations.filter(dest => dest.climate === climate && 
-            dest.type === type && dest.season === season)
-            const myString = JSON.stringify(selectedDestinations);
-        document.getElementById("result").innerHTML = myString;
-        selectedDestinations.forEach(dest => {
-            var marker = new google.maps.Marker();
-            marker.setPosition(new google.maps.LatLng(dest.location.lat,dest.location.lng));
-            marker.setMap(map);
-        })
+    //Use user input to filter through array.
+    const selectedDestinations = destinations.filter(dest => dest.climate === climate &&
+        dest.type === type && dest.season === season);
+    console.log(typeof (selectedDestinations));
+    //Use a forEach loop to cycle through array and output the name parameter.
+    selectedDestinations.forEach(function (destinations) {
+        var x = destinations.name;
+        document.getElementById("result").innerHTML += `<br>${x}`;
+    });
+    // Use a forEach loop to cycle through all 'selectedDestinations' and create a marker in goolge maps for each output.
+    selectedDestinations.forEach(dest => {
+        var marker = new google.maps.Marker();
+        marker.setPosition(new google.maps.LatLng(dest.location.lat, dest.location.lng));
+        marker.setMap(map);
+    });
 }
-
